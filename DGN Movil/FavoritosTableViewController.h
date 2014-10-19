65dca1104cc38ca6bf932bf6411ffcec4baee5b1
Juan <juanjoseen@gmail.com>
2014-10-19 04:17:25 -0500

    empezando favoritos

---
+++ DGN Movil/FavoritosTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoritosTableViewController.h
+//  DGN Movil
+//
+//  Created by Juan on 19/10/14.
+//  Copyright (c) 2014 AxkanSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritosTableViewController : UITableViewController
+
+@end
